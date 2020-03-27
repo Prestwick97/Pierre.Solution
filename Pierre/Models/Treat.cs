f02ffc0a557a2c84dfa13233a76ce63c47d1ce0c
Prestwick97 <prestwick97@gmail.com>
2020-03-27 10:16:09 -0700

    populate Treat.cs model

---
+++ Pierre/Models/Treat.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+
+namespace Pierre.Models
+{
+  public class Treat
+  {
+    public Treat()
+    {
+      this.Flavors = new HashSet<TreatFlavor>();
+    }
+
+    public int TreatId { get; set; }
+    public string Name { get; set; }
+    public virtual ICollection<TreatFlavor> Flavors { get; set; }
+  }
+}
