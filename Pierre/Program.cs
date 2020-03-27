00c967963c3185aaab95f29ec5a149623b459238
Prestwick97 <prestwick97@gmail.com>
2020-03-27 13:02:37 -0700

    populate Program.cs

---
+++ Pierre/Program.cs
@@ -0,0 +1,20 @@
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+
+namespace Pierre
+{
+  public class Program
+  {
+    public static void Main(string[] args)
+    {
+      var host = new WebHostBuilder()
+        .UseKestrel()
+        .UseContentRoot(Directory.GetCurrentDirectory())
+        .UseIISIntegration()
+        .UseStartup<Startup>()
+        .Build();s
+
+      host.Run();
+    }
+  }
+}
