b92d7142c755a81f23c31aba1eaf4033ef06a038
Prestwick97 <prestwick97@gmail.com>
2020-03-27 15:23:49 -0700

    create flavor index view

--- Pierre/Migrations/20200327222020_Authorization.Designer.cs
+++ Pierre/Migrations/20200327222020_Authorization.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Pierre.Models;
 
 namespace Pierre.Migrations
 {
     [DbContext(typeof(PierreContext))]
-    partial class PierreContextModelSnapshot : ModelSnapshot
+    [Migration("20200327222020_Authorization")]
+    partial class Authorization
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Pierre/Migrations/20200327222020_Authorization.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Pierre.Migrations
+{
+    public partial class Authorization : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "Treats",
+                newName: "UserId");
+
+            migrationBuilder.RenameColumn(
+                name: "Description",
+                table: "Flavors",
+                newName: "FlavorName");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                table: "Treats",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "TreatName",
+                table: "Treats",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Treats_UserId",
+                table: "Treats",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Treats_AspNetUsers_UserId",
+                table: "Treats",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Treats_AspNetUsers_UserId",
+                table: "Treats");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Treats_UserId",
+                table: "Treats");
+
+            migrationBuilder.DropColumn(
+                name: "TreatName",
+                table: "Treats");
+
+            migrationBuilder.RenameColumn(
+                name: "UserId",
+                table: "Treats",
+                newName: "Name");
+
+            migrationBuilder.RenameColumn(
+                name: "FlavorName",
+                table: "Flavors",
+                newName: "Description");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Treats",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+    }
+}
